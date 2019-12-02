938e23ba5b608e1506aeed82505865ea98f3d31d
Mahmud989 <bodurovmahmud@gmail.com>
2019-12-02 22:40:16 +0400

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    TextView txtView;
+    EditText loginText;
+    EditText passwordText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+        txtView = (TextView) findViewById(R.id.text);
+        loginText = (EditText) findViewById(R.id.loginText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                txtView.setText("Go Back");
+                txtView.setTextSize(25);
+                Context context = getApplicationContext();
+                CharSequence text = "Username is " + loginText.getText().toString() +
+                        "\r\nPassword is " + passwordText.getText().toString();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+//                showToast();
+            }
+        });
+    }
+
+    public void showToast() {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("This is a custom toast");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        Button button = (Button) findViewById(R.id.button3);
+        if (button != null) {
+            txtView.setText("Toast Clicker");
+        }
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                txtView.setText("Toast Clicker");
+//            }
+//        });
+
+
+        toast.show();
+    }
+
+
+}
