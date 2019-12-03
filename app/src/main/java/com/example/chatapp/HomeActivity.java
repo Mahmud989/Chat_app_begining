98e7a7b7a764863ca4915b28afadee3e60ebc71a
Mahmud989 <bodurovmahmud@gmail.com>
2019-12-03 12:40:16 +0400

    new task connect with login edit and button purpose : clean view

--- app/src/main/java/com/example/chatapp/HomeActivity.java
+++ app/src/main/java/com/example/chatapp/HomeActivity.java
@@ -7,13 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
     TextView loginView;
+    TextView passwordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
         loginView = (TextView) findViewById(R.id.home_user_name);
-        if (loginView != null)
-            loginView.setText(getIntent().getExtras().getString("username", "null"));
+        passwordView = (TextView) findViewById(R.id.home_user_password);
+        loginView.setText(getIntent().getExtras().getString("username", "null"));
+        passwordView.setText(getIntent().getExtras().getString("password", "null"));
     }
 }
